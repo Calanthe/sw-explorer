b780c102f719b4ab49174945220649cc824bf42f
Zosia <korczzofia@gmail.com>
2021-05-31 12:53:53 +0200

    SW header added, changed styles to dark

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import './App.css';
 import SWCharacters from './js/components/SWCharacters';
 
+const url = window.location.protocol + "//" + window.location.host;
+
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      <header className="header">
+          <img src={`${url}/img/star.svg`} alt="Star" className="header-img"/>
+          <img src={`${url}/img/wars.svg`} alt="Wars" className="header-img"/>
       </header>
       <SWCharacters/>
     </div>
